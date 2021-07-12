802a9bccce19e917dc4655c01ffaa9d94eaa0aac
Princess <b.stevenson@hotmail.com>
2021-07-12 12:09:13 +0100

    Product & Manufacturer Classes written

---
+++ models/manufacturer.py
@@ -0,0 +1,5 @@
+class Manufacturer:
+    def __init__(self, manufacturers_name, country_origin, id = None):
+        self.manufacturers_name = manufacturers_name
+        self.country_origin = country_origin
+        self.id = id
---
+++ models/product.py
@@ -0,0 +1,11 @@
+class Product:
+
+    def __init__(self, product_name, product_type, product_description, stock_quantity, buying_cost, selling_price, product_manufacturer, id = None, ):
+        self.product_name = product_name
+        self.product_type = product_type
+        self.product_description = product_description
+        self.stock_quantity = stock_quantity
+        self.buying_cost = buying_cost
+        self.selling_price = selling_price
+        self.product_manufacturer = product_manufacturer
+        self.id = id
