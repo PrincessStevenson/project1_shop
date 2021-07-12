a6fd77620fb3ddef2d9928657b8c6ad0717696dc
Princess <b.stevenson@hotmail.com>
2021-07-12 12:02:09 +0100

    shop_manager.sql created and tables including data type written out

---
+++ db/shop_manager.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS manufacturers;
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE manufacturers (
+  id SERIAL PRIMARY KEY,
+  manufacturers_name VARCHAR(255),
+  country_origin VARCHAR(255)
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  product_name VARCHAR(255),
+  product_type VARCHAR(255),
+  product_description VARCHAR(255),
+  stock_quantity BOOL,
+  buying_cost smallmoney,
+  selling_price smallmoney,
+  product_manufacturer VARCHAR(255)
+);
