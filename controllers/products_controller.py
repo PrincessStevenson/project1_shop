626c49a3ed2e8e6dec885dbf6240b34e8bd76626
Princess <b.stevenson@hotmail.com>
2021-07-12 12:23:13 +0100

    restful routes added as comments in products controller

--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -4,3 +4,20 @@ from models.product import Product
 
 products_blueprint = Blueprint("products", __name__)
 
+# NEW
+# GET '/product/new'
+
+# CREATE
+# POST '/products'
+
+# SHOW
+# GET '/products/<id>'
+
+# EDIT
+# GET '/products/<id>/edit'
+
+# UPDATE
+# PUT '/products/<id>'
+
+# DELETE
+# DELETE '/products/<id>'
