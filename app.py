c09c865e901a260fdacdee02f65c5075f9103403
Princess <b.stevenson@hotmail.com>
2021-07-12 13:26:11 +0100

    app routes & matching html pages created and checked - all working

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/products')
+def product():
+    return render_template('products.html')
+
+@app.route('/products/new')
+def new_product():
+    return render_template('newproduct.html')
+
+@app.route('/manufacturers')
+def manufacturers():
+    return render_template('manufacturers.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
