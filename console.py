49f32df8c442a879e48c5ff00a25ee8f6a95e6f9
Princess <b.stevenson@hotmail.com>
2021-07-12 13:35:31 +0100

    manufacturers added into console

--- console.py
+++ console.py
@@ -1,3 +1,21 @@
 import pdb
 from models.manufacturer import Manufacturer
 from models.product import Product
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.product_repository as product_repository
+
+manufacturer_repository.delete_all()
+product_repository.delete_all()
+
+manufacturer1 = Manufacturer("Anne Winters", "Scotland")
+manufacturer_repository.save(manufacturer1)
+manufacturer2 = Manufacturer("LoveLemon", "England")
+manufacturer_repository.save(manufacturer2)
+manufacturer3 = Manufacturer("Chinese Whispers", "China")
+manufacturer_repository.save(manufacturer3)
+manufacturer4 = Manufacturer("Agent Arrested", "France")
+manufacturer_repository.save(manufacturer4)
+
+manufacturer_repository.select_all()
+
+product1 = Product
