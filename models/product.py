d1ea9160e8a3f18b6ab0602250dcd14710a73327
Princess <b.stevenson@hotmail.com>
2021-07-13 22:30:26 +0100

    evening playaround with elif in mode.product.py - didn't work

--- models/product.py
+++ models/product.py
@@ -10,5 +10,16 @@ class Product:
         self.product_manufacturer = product_manufacturer
         self.id = id
 
-    # def mark_outofstock(self)
-    #     self.stock_quantity = False
+    # def mark_outofstock(self):
+        
+    #     stock_quantity = self.stock_quantity
+        
+    #     if stock_quantity is <5:
+    #         print ("Low in Stock!") 
+            
+    #     elif stock_quantity == 0
+    #         print ("Out of Stock!")
+            
+    #     else: print ("In Stock!")
+    
+    # selling_price - buying_cost = 
